567daf9ef7e53a034e3d84cfb7bfd06004228b39
Marek Mościchowski <moscich@gmail.com>
2018-10-17 18:21:17 +0200

    Initial Commit

---
+++ AppLauncher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AppLauncher
+//
+//  Created by Marek Mościchowski on 17/10/2018.
+//  Copyright © 2018 Marek Mościchowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
